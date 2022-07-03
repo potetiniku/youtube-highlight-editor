4ae51084102e8902cff8e39c34bf27bb10badf23
potetiniku <potetiniku@gmail.com>
2022-07-03 21:50:46 +0900

    chore: 雛形とアイコンを作成

---
+++ YougeneHighlightEditor.Wpf/Windows/MainWindow/ViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YougeneHighlightEditor.Wpf.Windows.MainWindow;
+internal class ViewModel
+{
+}
