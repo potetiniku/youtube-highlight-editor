5ca1101f07ab9918b26e6b821da2d0dc61356e2b
potetiniku <potetiniku@gmail.com>
2022-07-03 21:03:18 +0900

    chore: CSV出力用のクラスを実装

---
+++ YougeneHighlightEditor.Model/Highlight.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration.Attributes;
+
+namespace YougeneHighlightEditor.Model;
+public class Highlight
+{
+	[Index(0), Name("配信日"), Format("yyyy-MM-dd")]
+	public DateTime DeliveredOn { get; set; }
+
+	[Index(1), Name("トリガー")]
+	public Trigger Trigger { get; set; }
+
+	[Index(2), Name("説明")]
+	public string Description { get; set; }
+
+	[Index(3), Name("YouTubeのURL")]
+	public Uri YouTubeUrl { get; set; }
+}
---
+++ YougeneHighlightEditor.Model/Trigger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration.Attributes;
+
+namespace YougeneHighlightEditor.Model;
+public enum Trigger
+{
+	[Name("その他")]
+	Other,
+
+	[Name("コメント")]
+	Comment,
+
+	[Name("コメント付きエール")]
+	CommentYell,
+
+	[Name("エール")]
+	Yell,
+
+	[Name("強者ボード")]
+	RankingBoard
+}
