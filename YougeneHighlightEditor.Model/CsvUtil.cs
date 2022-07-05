5d377aebccd2451b45f7ff379cf9d3ae12523f9c
potetiniku <potetiniku@gmail.com>
2022-07-05 11:49:25 +0900

    chore: CSVの入出力を実装

---
+++ YougeneHighlightEditor.Model/CsvUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+
+namespace YougeneHighlightEditor.Model;
+public class CsvUtil
+{
+	public static List<T> Read<T>(string sourcePath)
+	{
+		using StreamReader reader = new(sourcePath, Encoding.UTF8);
+		using CsvReader csvReader = new(reader, CultureInfo.InvariantCulture);
+		return csvReader.GetRecords<T>().ToList();
+	}
+
+	public static void OverWrite<T>(string destPath, IEnumerable<T> records)
+	{
+		using StreamWriter writer = new(destPath, false, Encoding.UTF8);
+		using CsvWriter csvWriter = new(writer, CultureInfo.InvariantCulture);
+		csvWriter.WriteRecords(records);
+	}
+}
